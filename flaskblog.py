939a17167587e1a0d6d565aab5175a51be2ff5fd
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-02 12:37:27 +0330

    Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
+posts = [
+    {
+        'author': 'Benyamin Mahmoudyan',
+        'title': 'Blog post 1',
+        'content':'fist content',
+        'date_posted': 'April 20, 2020',
+    },
+    {
+        'author': 'Benyamin Mahmoudyan',
+        'title': 'Blog post 2',
+        'content':'2nd content',
+        'date_posted': 'March 20, 2020',
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>about page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
