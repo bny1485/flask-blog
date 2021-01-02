7789e0a5f39e71281d7588f87c62c5f40ca36b84
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-02 23:42:22 +0330

    Forms and User Input

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fb29cf854cc3642cc510c2f7f7284ba6 '
+
 
 posts = [
     {
@@ -26,6 +29,29 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+        # url_for get function name
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@glog.com' and form.password.data == 'password':
+            flash(f'you haved be loged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'login unsuccessful. please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
