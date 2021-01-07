59feefeb484335afaef7add65fc097d21ca25830
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-07 16:50:11 +0330

    Blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+
+
+class Config():
+
+    SECRET_KEY = 'fb29cf854cc3642cc510c2f7f7284ba6 '
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'benyamin.zinet@gmail.com'
+    MAIL_PASSWORD = 'my email password'
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
 
 
-class RegisterationForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -48,12 +48,6 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That Email is already taken. Please Choose a different one.')
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 
 class RequestResetForm(FlaskForm):
