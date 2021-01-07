8b468d5dcea6848d33d2bac5a0f3dd93d3e5a325
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-07 13:35:02 +0330

    Email and Password Reset

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -16,14 +16,12 @@ class RegisterationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError(
-                'That username is already taken. Please Choose a different one.')
+            raise ValidationError('That username is already taken. Please Choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError(
-                'That Email is already taken. Please Choose a different one.')
+            raise ValidationError('That Email is already taken. Please Choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -43,18 +41,32 @@ class UpdateAccountForm(FlaskForm):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError(
-                    'That username is already taken. Please Choose a different one.')
+                raise ValidationError('That username is already taken. Please Choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError(
-                    'That Email is already taken. Please Choose a different one.')
+                raise ValidationError('That Email is already taken. Please Choose a different one.')
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm-Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
